a441fd6713e626157ce5db7f0462dd5bcd1eb5a9
Sego11 <sammybaffoe123@gmail.com>
2024-01-23 16:16:27 +0000

    added the weather model,service and screen

--- lib/models/weather.model.dart
+++ lib/models/weather.model.dart
@@ -3,8 +3,16 @@ class Weather {
   final double temperature;
   final String mainCondition;
 
-  Weather(
-      {required this.cityName,
-      required this.mainCondition,
-      required this.temperature});
+  Weather({
+    required this.cityName,
+    required this.mainCondition,
+    required this.temperature,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+        cityName: json['name'],
+        temperature: json['main']['temp'].toDouble(),
+        mainCondition: json['weather'][0]['main']);
+  }
 }
--- lib/views/weather.screen.dart
+++ lib/views/weather.screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather.model.dart';
+import 'package:weather_app/services/weather.service.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
   @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  //api key
+  final _weatherService = WeatherService('a17cca0a0ad95124d029bd93cdfac3e3');
+  Weather? _weather;
+
+  //fetch weather
+
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for the city
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //fetch weather
+    _fetchWeather();
+  }
+
+  //weather animation
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //city name
+            Text(_weather?.cityName ?? 'loading city...'),
+            //temperature
+            Text('${_weather?.temperature.round().toString()} ℃'),
+          ],
+        ),
+      ),
+    );
   }
 }
