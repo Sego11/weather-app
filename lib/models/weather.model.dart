75ffab09506f32b47aef8d17410506539b97dc84
Sego11 <sammybaffoe123@gmail.com>
2024-01-23 11:34:57 +0000

    getting started

---
+++ lib/models/weather.model.dart
@@ -0,0 +1,10 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather(
+      {required this.cityName,
+      required this.mainCondition,
+      required this.temperature});
+}
